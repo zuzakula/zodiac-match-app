682561f0819407b5180ec544dbd2fc0fc0ce57e5
zuzakula <zuzka.kula@gmail.com>
2023-09-05 14:56:10 +0200

    feat: email/password auth

--- App.tsx
+++ App.tsx
@@ -1,6 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
 import { createContext, useEffect, useState } from "react";
-import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from "@env";
 import * as Google from "expo-auth-session/providers/google";
@@ -11,36 +9,75 @@ import {
   onAuthStateChanged,
   signInWithCredential,
 } from "firebase/auth";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import firebase from "firebase/compat";
+import User = firebase.User;
 
 WebBrowser.maybeCompleteAuthSession();
 
 export const AuthContext = createContext<any>({ user: null });
 
+const Stack = createNativeStackNavigator();
+
+const InsideStack = createNativeStackNavigator();
+
+const InsideLayout = () => {
+  return (
+    <InsideStack.Navigator>
+      <InsideStack.Screen name="Home screen" component={HomeScreen} />
+    </InsideStack.Navigator>
+  );
+};
+
 export default function App() {
-  const [userInfo, setUserInfo] = useState();
+  const [user, setUser] = useState<any>(null);
   const [request, response, promptAsync] = Google.useAuthRequest({
     iosClientId: IOS_CLIENT_ID,
     androidClientId: ANDROID_CLIENT_ID,
   });
 
-  useEffect(() => {
-    if (response?.type === "success") {
-      const { id_token } = response.params;
-      const credential = GoogleAuthProvider.credential(id_token);
-      signInWithCredential(auth, credential).then((r) => console.log(r));
-    }
-  }, [response]);
+  // useEffect(() => {
+  //   if (response?.type === "success") {
+  //     const { id_token } = response.params;
+  //     const credential = GoogleAuthProvider.credential(id_token);
+  //     signInWithCredential(auth, credential).then((r) => console.log(r));
+  //   }
+  // }, [response]);
+  //
+  // useEffect(() => {
+  //   const unsub = onAuthStateChanged(auth, async (user: any) => {
+  //     user ? setUser(user) : console.log("err");
+  //   });
+  //   return () => unsub();
+  // }, []);
 
   useEffect(() => {
-    const unsub = onAuthStateChanged(auth, async (user: any) => {
-      user ? setUserInfo(user) : console.log("err");
+    onAuthStateChanged(auth, (user: any) => {
+      if (user) {
+        setUser(user);
+      }
     });
-    return () => unsub();
   }, []);
 
   return (
-    <View>
-      {userInfo ? <HomeScreen /> : <LoginScreen promptAsync={promptAsync} />}
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        {user ? (
+          <Stack.Screen
+            name="Inside"
+            component={InsideLayout}
+            options={{ headerShown: false }}
+          ></Stack.Screen>
+        ) : (
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            // options={{ headerShown: false }}
+          />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,10 +1,78 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, TextInput, ActivityIndicator } from "react-native";
+import { useState } from "react";
+import { auth } from "../firebaseConfig";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 
-const LoginScreen = ({ promptAsync }) => {
+const LoginScreen = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const firebaseAuth = auth;
+
+  const signIn = async () => {
+    setLoading(true);
+    try {
+      const res = await signInWithEmailAndPassword(
+        firebaseAuth,
+        email,
+        password
+      );
+      console.log(res);
+    } catch (err) {
+      alert("Sign in failed: " + err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const signUp = async () => {
+    setLoading(true);
+    try {
+      const res = await createUserWithEmailAndPassword(
+        firebaseAuth,
+        email,
+        password
+      );
+      console.log(res);
+    } catch (err) {
+      alert("Registration failed: " + err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <View>
-      <Text>login screen </Text>
-      <Button title="login" onPress={() => promptAsync()} />
+      <Text>L O G I N </Text>
+      <TextInput
+        placeholder="E-Mail"
+        autoCapitalize="none"
+        onChangeText={(v) => {
+          setEmail(v);
+        }}
+        value={email}
+      ></TextInput>
+      <TextInput
+        placeholder="Password"
+        autoCapitalize="none"
+        onChangeText={(v) => {
+          setPassword(v);
+        }}
+        value={password}
+        secureTextEntry={true}
+      ></TextInput>
+      {loading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <>
+          <Button title="Sign in" onPress={signIn} />
+          <Button title="Create account" onPress={signUp} />
+        </>
+      )}
+      {/*<Button title="login with google" onPress={() => promptAsync()} />*/}
     </View>
   );
 };
